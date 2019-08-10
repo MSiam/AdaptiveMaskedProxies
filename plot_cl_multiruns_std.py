4701e3521e4cfc9619df91006218d8eb7134523b
mennatullah siam <mennatul@ualberta.ca>
2019-08-10 17:07:30 -0600

    Multiple runs with std plot

--- plot_cl_multiruns_std.py
+++ plot_cl_multiruns_std.py
@@ -30,23 +30,37 @@ mious_runs = []
 for i, run in enumerate(runs):
     cl_paths = sorted(os.listdir(sys.argv[1]+str(i)+'/'))
     mious_list = []
-
     for j in range(len(cl_paths)):
         mious = parse_file(sys.argv[1]+str(i)+'/'+cl_paths[j])
         mious_list.append(mious)
     mious_runs.append(mious_list)
 
-styles = ['-b', '-g', '-m', '-r', '-c', '-y']
-legends = ['Naive #1', 'Naive #10', 'Imprint 0.05', 'Imprint 0.02', 'Imprint 0.01']
+styles = ['-b', '-r']
+legends = ['Naive #1', 'Imprint 0.01']
 
 mious_list = np.array(mious_runs).mean(0)
-for i, mious in enumerate(mious_list):
-    plt.plot(list(range(1,6)), mious, styles[i], marker='o')
+stds_list = np.array(mious_runs).std(0)
+clrs = ['blue', 'red']
+for i, (mious, stds) in enumerate(zip(mious_list, stds_list)):
+    if i == 0:
+        c = 0
+        plt.plot(list(range(1,6)), mious, styles[c], marker='o')
+        plt.fill_between(range(1,6), mious-stds, mious+stds, facecolor=clrs[c], alpha=0.2)
+    elif i == 4:
+        c = 1
+        plt.plot(list(range(1,6)), mious, styles[c], marker='o')
+        plt.fill_between(range(1,6), mious-stds, mious+stds, facecolor=clrs[c], alpha=0.2)
+
+axes = plt.subplot(1, 1, 1)
+plt.xlabel('Encountered Tasks')
+plt.ylabel('mIoU')
 
 plt.xticks(range(1,6))
 plt.xlabel('Encountered Tasks')
 plt.ylabel('mIoU')
 plt.legend(legends)
 plt.title('iPASCAL: Continually Learning 2 New Classes in each Task')
-plt.savefig('plot_cl.png')
+
+#plt.show()
+plt.savefig('ipascal_std.png')
 
